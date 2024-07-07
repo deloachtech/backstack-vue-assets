e348955c70280a045821999fc1261895327e7ac9
Aaron DeLoach <aarondeloach@gmaiil.com>
2024-07-07 13:44:35 -0500

    alpha update

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
-  <div ref="modalRef_xyh3476fcif63ife8" :data-bs-backdrop="backdrop" :data-bs-keyboard="keyboard" class="modal fade" id="modalId-xyh3476fcif63ife8" tabindex="-1" aria-hidden="true">
+  <div ref="modalRef_xyh3476fcif63ife8" :data-bs-backdrop="backdrop" :data-bs-keyboard="keyboard" class="modal fade bva-modal" id="modalId-xyh3476fcif63ife8" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5">{{ heading }}</h1>
-          <button @click.prevent="emit('cancel', item)" type="button" class="btn-close" aria-label="Close"></button>
+          <h1 v-if="heading" class="modal-title fs-5">{{ heading }}</h1>
+          <button @click.prevent="emit('cancel')" type="button" class="btn-close" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <div class="mb-3">
@@ -12,9 +12,9 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button @click.prevent="emit('cancel', item)" type="button" class="btn btn-secondary">Cancel</button>
-          <button v-if="loading" type="button" disabled class="btn btn-primary"><span class="spinner-border spinner-border-sm" aria-hidden="true"></span> Submit</button>
-          <button v-else type="submit" @click.prevent="emit('submit', item)" class="btn btn-primary">Submit</button>
+          <button @click.prevent="emit('cancel')" type="button" class="btn btn-secondary">Cancel</button>
+          <button v-if="submitting" type="button" disabled class="btn btn-primary"><span class="spinner-border spinner-border-sm" aria-hidden="true"></span> Submit</button>
+          <button v-else type="submit" @click.prevent="emit('submit')" class="btn btn-primary">Submit</button>
         </div>
       </div>
     </div>
@@ -30,41 +30,12 @@ const emit = defineEmits(["submit", "cancel"]);
 const modalRef_xyh3476fcif63ife8 = ref(null);
 
 const props = defineProps({
-  open: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  item: {
-    type: Object,
-    required: true,
-    default: {},
-  },
-  loading: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  heading: {
-    type: String,
-    required: false,
-    default: "Update Value",
-  },
-  static: {
-    type: Boolean,
-    required: false,
-    default: true,
-  },
-  backdrop: {
-    type: [Boolean, String],
-    required: false,
-    default: "static",
-  },
-  keyboard: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
+  open: { type: Boolean, required: false, default: false },
+  submitting: { type: Boolean, required: false, default: false },
+  heading: String,
+  static: { type: Boolean, required: false, default: true },
+  backdrop: { type: [Boolean, String], required: false, default: "static" },
+  keyboard: { type: Boolean, required: false, default: false },
 });
 
 const openModal = () => {
